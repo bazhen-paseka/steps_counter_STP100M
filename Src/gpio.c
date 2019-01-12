8280114e4279f5099e9a73adedd204afda112243
Бажен Левковець <bazhen.paseka@gmail.com>
2019-01-12 20:23:00 +0200

    add pull down on input pins

--- Src/gpio.c
+++ Src/gpio.c
@@ -73,7 +73,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pins : PA0 PA1 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PtPin */
